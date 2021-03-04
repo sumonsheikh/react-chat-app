0dc99f2c744417a10bf2aae80b9d7a1808ec2727
sumonsheikh <sm.sumon117@gmail.com>
2021-03-04 20:57:39 +0600

    create chat ui install dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {ChatEngine} from 'react-chat-engine'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChatEngine
+      projectID="22095367-729e-4e95-a0fd-eeeb64ffee7d
+      "
+      userName="sumonshekh"
+      userSecret="123456"
+
+    />
+  )
 }
 
-export default App;
+export default App
+
