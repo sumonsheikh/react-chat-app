2857d1a5f159497341e8c0e2bcafd38706bf5f6f
sumonsheikh <sm.sumon117@gmail.com>
2021-03-06 23:22:59 +0600

    add custom chat feed component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {ChatEngine} from 'react-chat-engine'
+import NewChatFeed from './components/NewChatFeed'
 
 const App = () => {
   return (
     <ChatEngine
+      height="100vh"
       projectID="22095367-729e-4e95-a0fd-eeeb64ffee7d
       "
       userName="sumonshekh"
       userSecret="123456"
+      renderChatFeed ={(chatProps)=>{<NewChatFeed {...chatProps}/>}}
 
     />
   )
---
+++ src/components/NewChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewChatFeed = (props) => {
+    console.log(props)
+    return (
+        <div>
+            New Chat feed
+        </div>
+    )
+}
+
+export default NewChatFeed
