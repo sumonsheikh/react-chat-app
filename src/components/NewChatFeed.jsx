3d556ddd7a5f4f632a0b764f05dda365c1ee316e
sumonsheikh <sm.sumon117@gmail.com>
2021-03-07 23:36:32 +0600

    crate renderMessage function

--- src/components/NewChatFeed.jsx
+++ src/components/NewChatFeed.jsx
@@ -4,15 +4,37 @@ import MyMessage from './MyMessage'
 import TheirMessage from './TheirMessage'
 
 const NewChatFeed = (props) => {
-    const {chats, activeChat, message, userName } = props
+    const {chats, activeChat, messages, userName } = props
     const chat = chats && chats[activeChat]
-    console.log(chat, userName, message)
+    
+    const renderMessage =()=>{
+        const keys = Object.keys(messages)
+        return keys.map((key, index)=>{
+            const message = messages[key]
+            const lastMessage = index === 0 ? null : keys[index-0]
+            const isMyMessage = userName === message.sender.username
+            return(
+                <div key={`msg_${index}`} style={{widht:`100%`}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ?<MyMessage/>
+                            :<TheirMessage/>
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage? '18px' : '0px ',  marginLeft: isMyMessage? '0px' : '68px' }}>
+                        read receipts
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessage()
     return (
-        <>
-          <MyMessage/>
-          <TheirMessage/>
-          <MessageForm/>
-        </>
+        <div>
+            chat feed
+        </div>
     )
 }
 
